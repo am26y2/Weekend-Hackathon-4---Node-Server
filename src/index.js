7c75c6d78838a592c69022f41f4db6ae584dcd62
am26y2 <amitraj9046@gmail.com>
2020-11-12 10:17:11 +0530

    change

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to Dominos!");
+  } else if (req.url === "/contact") {
+    const contactObj = {
+      phone: "18602100000",
+      email: "guestcaredominos@jublfood.com",
+    };
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(contactObj));
+  } else {
+    res.writeHead(404).end("page not found");
+  }
 }
+
+httpServer.listen(8081, () => {
+  console.log("Listning");
+});
 
 module.exports = httpServer;
